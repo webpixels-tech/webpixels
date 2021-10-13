92f83c19d03068f7126ea051eeaab0ccf29f2b03
mkverma541 <mkverma541@gmail.com>
2021-10-13 09:33:54 +0530

    witty template

--- templates/Wittytech/assets/js/script.js
+++ templates/Wittytech/assets/js/script.js
@@ -2,3 +2,18 @@ const toggle = document.getElementById("toggle");
 toggle.addEventListener('click', function() {
     toggle.classList.toggle('active');
 })
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 400) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
